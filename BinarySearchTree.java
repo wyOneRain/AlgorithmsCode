2e043f95cabac3b60f2ab5b793c45b5e98dc17e2
wyOneRain <1015933411@qq.com>
2018-03-15 21:55:49 +0800

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,9 +1,7 @@
-import java.util.Arrays;
+import java.util.Stack;
 
-public class Practice {
-
-    //二叉树节点类
-    public static class  Node{
+public class BST {
+    private  class  Node{
         public Node left,right;
         public int data;
 
@@ -15,158 +13,95 @@ public class Practice {
         }
     }
 
-    public static class Solutions{
-        
-        //根据前序和中序重构二叉树
-        public Node reConstructBinaryTree(int [] pre,int [] in) {
-            if (pre.length==0 || in.length==0)
-                return null;
-            
-            Node root = new Node(pre[0]);
-            
-            //获得中序根节点位置
-            int index = 0;
-            for(int i = 0; i<in.length;i++){
-                if (in[i] == pre[0]){
-                    index = i;
-                    break;
-                }
-            }
-            
-         
-            root.left=reConstructBinaryTree(Arrays.copyOfRange(pre,1,index+1),Arrays.copyOfRange(in,0,index));
-            root.right=reConstructBinaryTree(Arrays.copyOfRange(pre,index+1,pre.length),Arrays.copyOfRange(in,index+1,in.length));
+    public Node root;
 
-            return root;
+    //查找指定节点
+    public Node find(int key) {
+        Node currentNode = root;
+        while (currentNode != null && currentNode.data != key) {
+            if (key < currentNode.data)
+                currentNode = currentNode.left;
+            else
+                currentNode = currentNode.right;
         }
-        
-         //获取根节点到某一个节点的路径
-        public boolean getNodePath(Node Tree,int key,Stack path){
-            path.push(Tree.data);
-
-            if (Tree.data == key)
-                return true;
-
-            boolean found = false;
-            if (Tree.left != null)
-                found = getNodePath(Tree.left,key,path);
-            if (!found && Tree.right != null)
-                found = getNodePath(Tree.right,key,path);
-            if (!found)
-                path.pop();
-
-            return found;
-        }
-
-        //前序遍历{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}
-        private Node reConstructBinaryTree(int [] pre,int startPre,int endPre,int [] in,int startIn,int endIn) {
-
-            if(startPre>endPre||startIn>endIn)
-                return null;
-            Node root=new Node(pre[startPre]);
-
-            for(int i=startIn;i<=endIn;i++)
-                if(in[i]==pre[startPre]){
-                    root.left=reConstructBinaryTree(pre,startPre+1,startPre+i-startIn,in,startIn,i-1);
-                    root.right=reConstructBinaryTree(pre,i-startIn+startPre+1,endPre,in,i+1,endIn);
-                }
-            return root;
-        }
-
+        return currentNode;
     }
 
-    //创建二叉搜索树
-    public static class BinarySearchTree {
-        //定义树的根节点
-        public Node root;
-
-        //查找指定节点
-        public Node find(int key) {
-            Node currentNode = root;
-            while (currentNode != null && currentNode.data != key) {
-                if (key < currentNode.data)
-                    currentNode = currentNode.left;
-                else
-                    currentNode = currentNode.right;
-            }
-            return currentNode;
+    //插入节点
+    public void insert(int key) {
+        if (root == null) {
+            root = new Node(key);
+            return;
         }
-
-        //插入节点
-        public void insert(int key) {
-            if (root == null) {
-                root = new Node(key);
-                return;
-            }
-            Node currentNode = root;
-            while (true) {
-                if (currentNode.data > key) {
-                    if (currentNode.left == null) {
-                        currentNode.left = new Node(key);
-                        return;
-                    } else {
-                        currentNode = currentNode.left;
-                    }
+        Node currentNode = root;
+        while (true) {
+            if (currentNode.data > key) {
+                if (currentNode.left == null) {
+                    currentNode.left = new Node(key);
+                    return;
                 } else {
-                    if (currentNode.right == null) {
-                        currentNode.right = new Node(key);
-                        return;
-                    } else {
-                        currentNode = currentNode.right;
-                    }
+                    currentNode = currentNode.left;
                 }
-            }
-        }
-
-        //先序遍历
-        public void preOrder(Node rootNode) {
-            if (rootNode != null) {
-                System.out.print(rootNode.data + " ");
-                preOrder(rootNode.left);
-                preOrder(rootNode.right);
-            }
-        }
-
-        //中序遍历
-        public void inOrder(Node rootNode) {
-            if (rootNode != null) {
-                inOrder(rootNode.left);
-                System.out.print(rootNode.data + " ");
-                inOrder(rootNode.right);
-            }
-        }
-
-        //后序遍历
-        public void postOrder(Node rootNode) {
-            if (rootNode != null) {
-                postOrder(rootNode.left);
-                postOrder(rootNode.right);
-                System.out.print(rootNode.data + " ");
+            } else {
+                if (currentNode.right == null) {
+                    currentNode.right = new Node(key);
+                    return;
+                } else {
+                    currentNode = currentNode.right;
+                }
             }
         }
     }
 
-    public static void main(String[] args) {
-//        BinarySearchTree tree = new BinarySearchTree();
-//        int[] list = new int[]{7, 5, 2, 4, 6, 8, 1, 9, 11, 3, 12, 10};
-//        for (int x : list) {
-//            tree.insert(x);
-//        }
-//        tree.preOrder(tree.root);
-//        System.out.println(" ");
-//        tree.inOrder(tree.root);
-//        System.out.println(" ");
-//        tree.postOrder(tree.root);
-        int[] pre = new int[]{1,2,4,7,3,5,6,8};
-        int[] in = new int[]{4,7,2,1,5,3,8,6};
-        System.out.println(pre.length);
+    //递归先序遍历
+    public void preOrder(Node rootNode) {
+        Node currentNode = this.root;
+        if (rootNode != null) {
+            System.out.print(rootNode.data + " ");
+            preOrder(rootNode.left);
+            preOrder(rootNode.right);
+        }
+    }
 
+    //非递归先序遍历
+    public void preOrder(){
+        Stack<Node> s = new Stack<Node>();//保存节点信息-->先序遍历（根左右）类似栈，先进后出
+        
+        Node currentNode = this.root;//游标节点
+        
+        while (!s.isEmpty() || currentNode != null)
+        {
+            while (currentNode != null)//先将父节点的所有左子树推入栈
+            {
+                s.push(currentNode);
+                System.out.print(currentNode.data+" ");//输出节点值
+                currentNode=currentNode.left;
+            }
+            
+            //左子树遍历完成后，推出，到下一个右节点
+            if(!s.isEmpty()){
+                Node t = s.pop();
+                currentNode = t.right;
+            }
 
-        Node tree = new Solutions().reConstructBinaryTree(pre,in);
-        int[] newdata = Arrays.copyOfRange(pre,2,8);
-        for(int x:newdata)
-            System.out.println(x);
+        }
+    }
 
+    //中序遍历
+    public void inOrder(Node rootNode) {
+        if (rootNode != null) {
+            inOrder(rootNode.left);
+            System.out.print(rootNode.data + " ");
+            inOrder(rootNode.right);
+        }
+    }
 
+    //后序遍历
+    public void postOrder(Node rootNode) {
+        if (rootNode != null) {
+            postOrder(rootNode.left);
+            postOrder(rootNode.right);
+            System.out.print(rootNode.data + " ");
+        }
     }
 }
