570f72995b522e48238a77c46ad034d4022554fc
wyOneRain <1015933411@qq.com>
2018-03-18 20:47:45 +0800

    Update Valid Parentheses.py

--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -20,3 +20,25 @@ class Solution:
             else:
                 return False
         return stack == []
+    
+ '''
+ jAVA VESION:
+ 
+ class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for(char c : s.toCharArray()){
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
+ 
+ '''
