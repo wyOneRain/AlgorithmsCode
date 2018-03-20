7f24cd83695730798f4cd036589af5527686026a
wyOneRain <1015933411@qq.com>
2018-03-20 22:15:56 +0800

    Update and rename quckSorrt.java to quckSort.java

--- quckSort.java
+++ quckSort.java
@@ -2,6 +2,7 @@ class Solution {
     public void quckSort(int[] arr,int left,int right){
         if(left>right)
             return;
+        
         int i = left,j=right;
         int base = arr[left];//基准位
 
@@ -10,18 +11,22 @@ class Solution {
             //先看右边，依次往左递减
             while (base <= arr[j] && i<j)
                 j--;
+            
             //后看左边，依次往右递加
             while (base >= arr[i] && i<j)
                 i++;
+            
             if (i<j) {
                 int t = arr[j];
                 arr[j] = arr[i];
                 arr[i] = t;
             }
         }
+        
         int temp = arr[left];
         arr[left] = arr[i];
         arr[i] = temp;
+        
         quckSort(arr,left,j-1);
         quckSort(arr,j+1,right);
     }
