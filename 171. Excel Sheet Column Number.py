a0389047a290cb0221949c5a8253a161b7021f6a
wyOneRain <1015933411@qq.com>
2018-06-11 16:55:30 +0800

    Create 171. Excel Sheet Column Number.py

---
+++ 171. Excel Sheet Column Number.py
@@ -0,0 +1,35 @@
+'''
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+For example:
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+    ...
+Example 1:
+Input: "A"
+Output: 1
+Example 2:
+Input: "AB"
+Output: 28
+Example 3:
+Input: "ZY"
+Output: 701
+
+'''
+
+
+
+class Solution:
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        sum = 0
+        for i,ch in enumerate(s[::-1]):
+            sum = sum + (ord(ch)-64)*(26**i)
+        return sum
